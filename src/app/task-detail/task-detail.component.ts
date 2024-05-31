92014260cf4259ecc6fd806a1b711d2197addce2
Carlos Pacheco <acprd_carlinhos@hotmail.com>
2024-05-31 00:40:39 -0400

    inicial tela de detalhe

--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { Task } from '../models/task';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-task-detail',
   standalone: true,
-  imports: [],
+  imports: [MatFormField, MatCheckbox, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './task-detail.component.html',
-  styleUrl: './task-detail.component.css'
+  styleUrl: './task-detail.component.css',
 })
 export class TaskDetailComponent {
+  task: Task = { id: 1, title: 'Learn Angular', isCompleted: false };
 
+  submitForm() {}
 }
